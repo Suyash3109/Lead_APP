29088fe0acf40d2772b9b7b446925a1d1e604a00
top_dawg <tripathisuyash00@gmail.com>
2022-09-26 17:52:19 +0530

    search barr

--- lib/HomeScreen/Account.dart
+++ lib/HomeScreen/Account.dart
@@ -2,6 +2,7 @@ import 'package:e_commerce/Screens/Login_screen.dart';
 import 'package:e_commerce/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Account extends StatefulWidget {
   const Account({super.key});
@@ -39,6 +40,27 @@ class _AccountState extends State<Account> {
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(
+          child: Text(
+        " Hey, this is my homepage, so I have to say something about myself. Sometimes it is hard to introduce yourself because you know yourself so well that you do not know where to start with. Let me give a try to see what kind of image you have about me through my self-description. I hope that my impression about myself and your impression about me are not so different. Here it goes.",
+        style: GoogleFonts.playfairDisplay(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+      )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Padding(
+        padding: EdgeInsets.only(bottom: 100),
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: kPrimaryColor,
+          elevation: 10,
+          child: Icon(
+            Icons.add_circle_sharp,
+            color: kSecondaryColor,
           ),
         ),
       ),
--- lib/HomeScreen/Leads.dart
+++ lib/HomeScreen/Leads.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:e_commerce/HomeScreen/Details.dart';
 import 'package:e_commerce/MVC/Controller/controller.dart';
 import 'package:e_commerce/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Leads extends StatefulWidget {
   const Leads({super.key});
@@ -18,97 +20,127 @@ class _LeadsState extends State<Leads> {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController textController = TextEditingController();
+
     return Scaffold(
-        // appBar: AppBar(),
-        body: NestedScrollView(
-      // floatHeaderSlivers: true,
-      headerSliverBuilder: (context, innerBoxIsScrolled) => [
-        SliverAppBar(
-          shadowColor: kPrimaryColor,
+      // appBar: AppBar(),
+      body: NestedScrollView(
+        // floatHeaderSlivers: true,
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            shadowColor: kPrimaryColor,
 
-          // pinned: true,
-          // floating: true,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
-          toolbarHeight: 100,
-          // flexibleSpace: Text("wrsgfs"),
-          scrolledUnderElevation: 10.0,
-          elevation: 10,
-          actions: [
-            IconButton(
-                padding: EdgeInsets.only(right: 20),
-                onPressed: () {},
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.white,
-                  size: 30,
-                ))
-          ],
+            // pinned: true,
+            // floating: true,
+            shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(35))),
+            toolbarHeight: 100,
+            // flexibleSpace: Text("wrsgfs"),
+            scrolledUnderElevation: 10.0,
+            elevation: 10,
+            actions: [
+              AnimSearchBar(
+                  closeSearchOnSuffixTap: true,
+                  color: kSecondaryColor,
+                  helpText: "search here",
+                  width: 400,
+                  textController: textController,
+                  onSuffixTap: () {
+                    setState(() {
+                      textController.clear();
+                    });
+                  }),
+              SizedBox(
+                width: 10,
+              )
+            ],
 
-          backgroundColor: kPrimaryColor,
-          // centerTitle: true,
-          // leading: IconButton(
-          //   icon: Icon(
-          //     Icons.menu,
-          //     color: Colors.white,
-          //   ),
-          //   onPressed: (() {}),
-          // ),
-          title: Text(
-            "L E A D S",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+            backgroundColor: kPrimaryColor,
+            // centerTitle: true,
+            // leading: IconButton(
+            //   icon: Icon(
+            //     Icons.menu,
+            //     color: Colors.white,
+            //   ),
+            //   onPressed: (() {}),
+            // ),
+            title: Text(
+              "L E A D S",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
           ),
-        ),
-      ],
-      body: Obx(
-        () => Visibility(
-            visible: !_controller.isLoaded.value,
-            replacement: const Center(
-              child: CircularProgressIndicator(),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                  itemCount: _controller.customerModel.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      // focusNode: FocusNode(debugLabel: "drcfvg"),
-                      onTap: () {
-                        Get.to(() => DetailsPage(
-                              Id: _controller.customerModel[index].id,
-                            ));
-                      },
-                      focusColor: Colors.red,
-                      child: Card(
-                        shadowColor: kPrimaryColor,
-                        color: kSecondaryColor,
-                        // ignore: prefer_const_constructors
-                        margin: EdgeInsets.all(5),
-                        elevation: 10,
-                        child: Column(
-                          children: [
-                            ListTile(
-                              title: Text(
-                                _controller.customerModel[index].name
-                                    .toString(),
-                                style: TextStyle(fontWeight: FontWeight.bold),
+        ],
+
+        body: Obx(
+          () => Visibility(
+              visible: !_controller.isLoaded.value,
+              replacement: const Center(
+                child: CircularProgressIndicator(),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                    itemCount: _controller.customerModel.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        // focusNode: FocusNode(debugLabel: "drcfvg"),
+                        onTap: () {
+                          Get.to(() => DetailsPage(
+                                Id: _controller.customerModel[index].id,
+                              ));
+                        },
+                        focusColor: Colors.red,
+                        child: Card(
+                          shadowColor: kPrimaryColor,
+                          color: kSecondaryColor,
+                          // ignore: prefer_const_constructors
+                          margin: EdgeInsets.all(5),
+                          elevation: 10,
+                          child: Column(
+                            children: [
+                              ListTile(
+                                title: Text(
+                                  _controller.customerModel[index].name
+                                      .toString(),
+                                  style: GoogleFonts.playfairDisplay(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25),
+                                ),
+                                subtitle: Text(
+                                  _controller.customerModel[index].email
+                                      .toString(),
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
                               ),
-                              subtitle: Text(
-                                _controller.customerModel[index].email
-                                    .toString(),
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  }),
-            )),
+                      );
+                    }),
+              )),
+        ),
       ),
-    ));
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Padding(
+        padding: EdgeInsets.only(bottom: 80),
+        child: FloatingActionButton(
+          splashColor: Colors.orange,
+          highlightElevation: 20,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          onPressed: () {},
+          backgroundColor: kPrimaryColor,
+          elevation: 15,
+          child: Icon(
+            Icons.add_circle_sharp,
+            color: kSecondaryColor,
+          ),
+        ),
+      ),
+    );
   }
 }
